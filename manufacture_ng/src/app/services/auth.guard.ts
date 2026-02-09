1e4a7659742734031dd9fdfdb09ec47295e1e580
hanhnguyen-lion <hanhnguyen.lion@gmail.com>
2026-02-09 18:48:52 +0700

    fix login

--- manufacture_ng/src/app/services/auth.guard.ts
+++ manufacture_ng/src/app/services/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, GuardResult, MaybeAsync, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, Router} from '@angular/router';
 import { AuthService } from './auth.service';
 import { Observable } from 'rxjs';
 
@@ -15,15 +15,12 @@ export class AuthGuard implements CanActivate{
     
   }
 
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree{
+  canActivate(): Observable<boolean> | Promise<boolean> | boolean{
     if (this.authService.getToken){
         return true;
     }
     else{
-        this.router.createUrlTree(["/Login"]);
+        this.router.navigate(["/Login"]);
         return false;
     }
   }
